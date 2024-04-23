8ebb6afea364d3932e0436bdaad0a9091cd7ea7c
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-04-24 05:49:06 +0800

    Add prerequisites (#2)
    Includes deploying the project to Vercel, adding necessary files to 
    `.gitignore`, implementing linting using ESLint, code formatting with
    Prettier, bundling with Webpack, and testing with Jest. It also sets up
    automation workflows, templates for issues and pull requests, and a pull
    request labeler.

--- public/index.js
+++ public/index.js
@@ -1,28 +1,28 @@
-const button = document.getElementById('learn-more');
+const button = document.getElementById('learn-more')
 
 button.addEventListener('click', () => {
 	window.location.href =
-		'https://github.com/free-bird-operation-project/quiz-manager';
-});
+		'https://github.com/free-bird-operation-project/quiz-manager'
+})
 
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector('.cursor')
 
 document.addEventListener('mousemove', (e) => {
 	cursor.setAttribute(
 		'style',
 		'top: ' + (e.pageY - 10) + 'px; left: ' + (e.pageX - 10) + 'px;'
-	);
-});
+	)
+})
 
 document.addEventListener('click', () => {
-	cursor.classList.add('expand');
+	cursor.classList.add('expand')
 
 	setTimeout(() => {
-		cursor.classList.remove('expand');
-	}, 500);
-});
+		cursor.classList.remove('expand')
+	}, 500)
+})
 
-const emoji = document.getElementById('emoji');
+const emoji = document.getElementById('emoji')
 const emojiList = [
 	'😀',
 	'😉',
@@ -42,17 +42,17 @@ const emojiList = [
 	'🥹',
 	'🫣',
 	'🤓'
-];
+]
 
 function changeEmoji() {
-	const randomIndex = Math.floor(Math.random() * emojiList.length);
-	const newEmoji = emojiList[randomIndex];
+	const randomIndex = Math.floor(Math.random() * emojiList.length)
+	const newEmoji = emojiList[randomIndex]
 
-	emoji.style.transform = 'scale(0.1)';
+	emoji.style.transform = 'scale(0.1)'
 	setTimeout(() => {
-		emoji.textContent = newEmoji;
-		emoji.style.transform = 'scale(1)';
-	}, 100);
+		emoji.textContent = newEmoji
+		emoji.style.transform = 'scale(1)'
+	}, 100)
 }
-changeEmoji();
-setInterval(changeEmoji, 3000);
+changeEmoji()
+setInterval(changeEmoji, 3000)
